de46dfde434652cf73fa516d10aa138cc9e3886d
5hubham5ingh <ss.dev.me@gmail.com>
2025-06-26 15:17:39 +0530

    Add: '-p' option to print the result of expresion evaluation

--- js.js
+++ js.js
@@ -1,7 +1,19 @@
 import * as std from "std"
 import * as os from "os"
 
-const expression = scriptArgs.slice(1, scriptArgs.length).join('')
+let p;
+let scriptArgsToUse;
+
+if (scriptArgs[1] === "-p") {
+  p = (...all) => print(...all);
+  scriptArgsToUse = scriptArgs.slice(2, scriptArgs.length);
+} else {
+  p = () => { };
+  scriptArgsToUse = scriptArgs.slice(1, scriptArgs.length);
+}
+
+const expression = scriptArgsToUse.join('');
+
 globalThis.std = std
 globalThis.os = os
 globalThis.stdin = std.in.readAsString()
@@ -11,8 +23,5 @@ globalThis.pwd = os.getcwd()[0]
 globalThis.ls = os.readdir(pwd)[0].filter(f => f != '.' && f != '..')
 
 Array.prototype.for = function(cb) { for (const e of this) cb(e) }
-std.evalScript(expression)
 
-
-
-
+p(std.evalScript(expression)); 
