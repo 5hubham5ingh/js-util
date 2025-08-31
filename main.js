22b711db3c28a0b907e985cb408e42102bf6603c
ss <ss.dev.me@gmail.con>
2025-08-31 12:24:22 +0530

    Update: put error on stderr

--- main.js
+++ main.js
@@ -606,36 +606,38 @@ const scriptPath = args[0];
 
 const [st, err] = scriptPath ? os.stat(scriptPath) : [null, -1];
 
-if (!err && (st.mode & os.S_IFMT) === os.S_IFREG) {
-  const file = std.open(scriptPath, 'r');
-  if (!file) throw new Error(`Could not open file: ${scriptPath}`);
-  const fileContent = file.readAsString();
-  file.close();
-  std.evalScript(fileContent, { async: true });
+try {
+  if (!err && (st.mode & os.S_IFMT) === os.S_IFREG) {
+    const file = std.open(scriptPath, 'r');
+    if (!file) throw new Error(`Could not open file: ${scriptPath}`);
+    const fileContent = file.readAsString();
+    file.close();
+    std.evalScript(fileContent);
 
-} else if (args.length === 0) {
-  if (!isatty()) {
-    const expression = std.in.readAsString();
-    std.evalScript(expression, { async: true });
-  } else {
-    globalThis.history = [];
-    Object.defineProperty(globalThis, 'clear', {
-      get() { printf("\x1b[2J\x1b[H"); },
-    });
+  } else if (args.length === 0) {
+    if (!isatty()) {
+      const expression = std.in.readAsString();
+      std.evalScript(expression);
+    } else {
+      globalThis.history = [];
+      Object.defineProperty(globalThis, 'clear', {
+        get() { printf("\x1b[2J\x1b[H"); },
+      });
 
-    while (true) {
-      printf("❯ ");
-      const expression = std.in.getline();
-      if (expression === null) break;
-      history.push(expression);
-      try {
+      while (true) {
+        printf("❯ ");
+        const expression = std.in.getline();
+        if (expression === null) break;
+        history.push(expression);
         std.evalScript(expression);
-      } catch (error) {
-        print(error)
       }
     }
+  } else {
+    const expression = args.join(' ');
+    std.evalScript(expression);
   }
-} else {
-  const expression = args.join(' ');
-  std.evalScript(expression, { async: true });
+} catch (error) {
+  std.err.puts(
+    `${error.constructor.name}: ${error.message}\n${error.stack}`,
+  );
 }
